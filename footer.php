de829c98a925f6cf57d781097178c55bfcb3cf50
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2018-07-27 19:08:09 -0400

    Changing footer and adding background styles

---
+++ footer.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package understrap
+ */
+
+$the_theme = wp_get_theme();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
+
+<div class="wrapper" id="wrapper-footer">
+
+	<div class="<?php echo esc_attr( $container ); ?>">
+
+		<div class="row">
+
+			<div class="col-md-12">
+
+				<footer class="site-footer" id="colophon">
+
+					<div class="site-info">
+					<p>Copyright © <?php echo esc_html( date( 'Y' ) ); ?> | <?php echo esc_html( get_bloginfo( $show, 'display' ) ); ?> | All Rights Reserved</p>
+					</div><!-- .site-info -->
+
+				</footer><!-- #colophon -->
+
+			</div><!--col end -->
+
+		</div><!-- row end -->
+
+	</div><!-- container end -->
+
+</div><!-- wrapper end -->
+
+</div><!-- #page we need this extra closing tag here -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
+
